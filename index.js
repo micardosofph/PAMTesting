1ad7264ed810eda3bc0eb7b81e6a4cff5f2968d2
Miguel Cardoso <micardosofph@gmail.com>
2026-04-17 11:12:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import Page1 from './src/screens/HomeScreen';
+import Page2 from './src/screens/Page2';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+
+import Routes from "./src";
+
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Home: Page1,
+    About: Page2,
+  })
+);
+
 registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => Routes);
+
+export default Routes;
+
+
